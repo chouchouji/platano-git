567afcbff6b5c099549f70edc38d8e13490f5f31
chouchouji <1305974212@qq.com>
2024-09-20 17:47:06 +0800

    refactor: optimize code

--- src/bin.js
+++ src/bin.js
@@ -1,15 +1,13 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
-import fse from 'fs-extra'
-import { resolve } from 'path'
-import { fileURLToPath } from 'url'
+import { readJSONSync } from 'fs-extra/esm'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { runBranchCommand } from './commands/branch.js'
 import { runPushCommand } from './commands/push.js'
 import { runPullCommand } from './commands/pull.js'
 import { runCheckoutCommand } from './commands/checkout.js'
 import { runSwitchCommand } from './commands/switch.js'
-
-const { readJSONSync } = fse
 
 const packageJson = readJSONSync(resolve(fileURLToPath(new URL('.', import.meta.url)), '../package.json'))
 
