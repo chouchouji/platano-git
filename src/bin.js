36f33158b51694d9406d734dddbffee878c64d40
chouchouji <1305974212@qq.com>
2024-10-12 08:04:40 +0800

    refactor: remove useless option for branch command

--- src/bin.js
+++ src/bin.js
@@ -29,7 +29,6 @@ program
   .option('-dr', '删除本地和对应的远端分支')
   .option('-a', '查看所有分支')
   .option('-s', '选择远端名称')
-  .option('-v', '查看本地仓库中所有分支的详细信息')
   .option('-m [branch]', '修改分支名')
   .action(async (branch, options) => {
     await runBranchCommand(branch, options)
